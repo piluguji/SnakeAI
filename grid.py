a7c21c1a8ca21771d99031299b8563cdc47440a7
Aaryan Dhore <30392950+piluguji@users.noreply.github.com>
2021-09-11 16:48:07 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -5,6 +5,9 @@ from typing import Sized
 import pygame
 import tkinter as tk
 from tkinter import messagebox
+import copy
+
+from pygame import color
 
 class cube(object):
     def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
@@ -78,12 +81,17 @@ class snake(object):
                     self.turns.pop(pos)
 
             c.move()
+        
+        
 
     def reset(self, pos):
         pass
 
     def addCube(self):
-        pass
+        tail = self.body[-1]
+        newPos = (tail.pos[0] - tail.dirX, tail.pos[1] - tail.dirY)
+        newCube = cube(newPos, tail.dirX, tail.dirY)
+        self.body.append(newCube)
         
     def draw(self, surface):
         for i, c in enumerate(self.body):
@@ -108,19 +116,27 @@ def drawGrid(surface):
 def redrawWindow(surface):
     surface.fill((0,0,0)) #fills screen with black
     drawGrid(surface)
+    snack.draw(surface)
     s.draw(surface)
     pygame.display.update() #updates the screen
 
-def randomSnack(rows, item):
-    pass
+def randomSnack(snake):
+    positions = snake.body
 
+    needPos = True
+    while needPos:
+        x, y = (random.randint(0, rows - 1), random.randint(0, rows - 1))
+        for c in snake.body:
+            if (x,y) == c.pos:
+                break
+        else:
+            needPos = False
 
-def message_box(subject, content):
-    pass
+    return (x, y)
 
 
 def main():
-    global size, rows, spaceBetween, s, flag
+    global size, rows, spaceBetween, s, flag, snack
 
     size = 400
     rows = 20
@@ -131,16 +147,23 @@ def main():
     win = pygame.display.set_mode((size, size))
 
     s = snake((255, 0, 0), (10, 10))
+    snack = cube(randomSnack(s), color = (255, 255, 0))
 
     #Creating clock object
     clock = pygame.time.Clock()
 
     flag = True
+
     while flag: 
-        #pygame.time.delay(50) #adds manual delay to prevent from running too fast
+        pygame.time.delay(50) #adds manual delay to prevent from running too fast
         clock.tick(10) #ensures game runs at 10 fps
 
         s.move()
+
+        if s.head.pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(s), color = (255, 255, 0))
+
         redrawWindow(win)
 
         #Checking is X button is pressed to exit game
